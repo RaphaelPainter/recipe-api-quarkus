bbee8f6428df5fd8c89f1b905d07976ce18c2bc6
RaphaelPainter <raphael.painter@imt-atlantique.net>
2023-12-05 22:20:11 +0100

    auth works :D

---
+++ src/main/java/com/rpainter/controllers/AdminResource.java
@@ -0,0 +1,24 @@
+package com.rpainter.controllers;
+
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.*;
+
+@Path("/auth")
+@Authenticated
+public class AdminResource {
+    @GET
+    @Path("/permit-all")
+    @PermitAll
+    public String permitAll() {
+        return "ok";
+    }
+
+    @GET
+    @Path("/only-admin")
+    @RolesAllowed("admin")
+    public String onlyAdmin() {
+        return "ok";
+    }
+}
